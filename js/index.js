31e66bb65f5aae22e5a048f67c148aa5b1a25b36
Hellenfd <hfuentealba@gmail.com>
2024-09-09 19:22:00 -0300

    hola

---
+++ js/index.js
@@ -0,0 +1,25 @@
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function() {
+    this.innerText = "Cerrar Sesión";
+});
+
+function disappear(elemento){
+    let navegacion = document.querySelector('.add');
+    navegacion.remove();
+}
+
+var btn1 = document.getElementById("btn1");
+btn1.addEventListener("click", function(){
+    let textoActual = this.innerText;
+    let numeroActual = parseInt(textoActual);
+    numeroActual++;
+this.innerText = numeroActual + " me gusta";
+})
+
+var btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", function(){
+    let textoActual = this.innerText;
+    let numeroActual = parseInt(textoActual);
+    numeroActual++;
+this.innerText = numeroActual + " me gusta";
+})
